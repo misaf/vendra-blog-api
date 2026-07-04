7841b8d6c6063749a7a97767fcd54de570a336b6
Misaf <misaf.1990@gmail.com>
2026-07-05 00:28:53 +0330

    Align blog API route base URI

---
+++ tests/Feature/ApiRoutesTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use Misaf\VendraBlogApi\Tests\TestCase;
+
+pest()->extend(TestCase::class);
+
+it('registers blog api read routes', function (): void {
+    expect(Route::has('vendra-blog.blog-posts.index'))->toBeTrue()
+        ->and(Route::has('vendra-blog.blog-post-categories.index'))->toBeTrue()
+        ->and(route('vendra-blog.blog-posts.index', [], false))->toBe('/v1/blog-posts')
+        ->and(route('vendra-blog.blog-post-categories.index', [], false))->toBe('/v1/blog-post-categories');
+});
---
+++ tests/Feature/ServerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use Misaf\VendraBlogApi\JsonApi\V1\Server;
+use Misaf\VendraBlogApi\Tests\TestCase;
+
+pest()->extend(TestCase::class);
+
+it('uses the registered blog api base uri', function (): void {
+    $properties = (new ReflectionClass(Server::class))->getDefaultProperties();
+
+    expect($properties['baseUri'])->toBe('/v1');
+});
