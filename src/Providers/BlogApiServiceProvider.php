a8758f82245b8bebc05772552c741ee35d048a5e
misaf <misaf.1990@gmail.com>
2026-07-11 20:48:24 +0000

    wip

--- src/Providers/BlogApiServiceProvider.php
+++ src/Providers/BlogApiServiceProvider.php
@@ -6,7 +6,6 @@ namespace Misaf\VendraBlogApi\Providers;
 
 use Illuminate\Foundation\Console\AboutCommand;
 use Misaf\VendraBlogApi\JsonApi\V1\Server as BlogServer;
-use Misaf\VendraMultimediaApi\JsonApi\V1\Server as MultimediaServer;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -21,7 +20,6 @@ final class BlogApiServiceProvider extends PackageServiceProvider
     public function packageRegistered(): void
     {
         config()->set('jsonapi.servers.vendra-blog', config('jsonapi.servers.vendra-blog', BlogServer::class));
-        config()->set('jsonapi.servers.vendra-multimedia', config('jsonapi.servers.vendra-multimedia', MultimediaServer::class));
     }
 
     public function packageBooted(): void
